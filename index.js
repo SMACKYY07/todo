93866a5e4f55bec07cc715d1602df3d5df086588
SMACKYY07 <mohilharma@1414@gmail.com>
2026-02-22 20:17:48 +0530

    feat: initialize React project with Vite and Tailwind CSS
    - Added package.json with scripts and dependencies for React, Vite, and Tailwind CSS.
    - Created main application file (App.jsx) with basic todo input form.
    - Added SVG assets for Vite and React logos.
    - Included Tailwind CSS in the index.css file.
    - Set up main entry point (main.jsx) for rendering the React application.
    - Configured Vite with React and Tailwind CSS plugins in vite.config.js.

--- index.js
+++
@@ -1,4 +0,0 @@
-let a=109;
-let b = 374;
-
-console.log(a+b);
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react'
+
+export default function App() {
+  const[todos, setTodos]= useState()
+  return (
+   <div>
+    <form action="">
+      <input type="text" />
+      <button>add todo</button>
+    </form>
+   </div>
+  )
+}
+
+
